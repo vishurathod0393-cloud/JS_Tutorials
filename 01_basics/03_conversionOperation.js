f5738bbc5b69c539f28f7abfc3c572a172f3098d
vishurathod0393-cloud <vishurathod0393@gmail.com>
2026-07-26 18:53:12 +0530

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -40,3 +40,45 @@ console.log(numToString)
 true => 1; false => 0
 
 */
+
+
+
+// ************************** Operation ****************************
+
+console.log("************************** Operation ****************************")
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+let str1 = "hello"
+let str2 = " Vishal"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// console.log(+true)
+// console.log(+"")
+
+// console.log(num1)
+// console.log(num2)
+
+let gameCounter = 100
+++gameCounter;
+
+console.log(gameCounter);
+
+gameCounter++
+
+console.log(gameCounter);
