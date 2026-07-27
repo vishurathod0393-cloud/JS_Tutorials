7a382d66e8462a1fdc47153aa781d6a6219f1b34
vishurathod0393-cloud <vishurathod0393@gmail.com>
2026-07-28 00:30:11 +0530

    Iife function

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai() {
+    console.log(`DB connected`)
+})();
+
+
+(function aurcode(name) {
+    console.log(`DB CONNECTED TWO ${name}`)
+})('vishal');
